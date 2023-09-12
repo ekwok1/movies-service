fe8756b8639ef093f827ae15b3b1c95d54247e77
ekwok <ekwok@relias.com>
2023-09-11 23:01:50 -0400

    Get all movies

--- src/main/java/dev/ekwok/movies/Movie.java
+++ src/main/java/dev/ekwok/movies/Movie.java
@@ -10,7 +10,7 @@ import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
 import java.util.List;
 
-@Document(collation = "movies")
+@Document(collection = "movie")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
