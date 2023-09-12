12b08e36113e21ab36088084b652ccc007457250
ekwok <ekwok@relias.com>
2023-09-11 23:16:28 -0400

    Get a single movie

--- src/main/java/dev/ekwok/movies/MovieService.java
+++ src/main/java/dev/ekwok/movies/MovieService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -12,5 +13,9 @@ public class MovieService {
 
     public List<Movie> allMovies() {
         return movieRepository.findAll();
+    }
+
+    public Optional<Movie> singleMovie(String imdbId) {
+        return movieRepository.findMovieByImdbId(imdbId);
     }
 }
