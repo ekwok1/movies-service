491e5daffad9a631da7dea8028c1f35df73e04c5
ekwok <ekwok@relias.com>
2023-09-12 22:14:06 -0400

    Remove unused imports

--- src/main/java/dev/ekwok/movies/MovieController.java
+++ src/main/java/dev/ekwok/movies/MovieController.java
@@ -1,7 +1,5 @@
 package dev.ekwok.movies;
 
-import org.apache.coyote.Response;
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
